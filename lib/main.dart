e3bdf02fc7d232dde314fd98d1cf730346bf777f
Amber Jain <jainanuamber@gmail.com>
2022-05-27 20:58:49 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trumeet/screens/home_screen.dart';
 import 'package:trumeet/screens/login_screen.dart';
 import 'package:trumeet/utils/colors.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

94d891a49ee6ce007ebbde5510581096eeebc8f3
Amber Jain <jainanuamber@gmail.com>
2022-05-28 19:56:23 +0530

    added video_call_screen

---
+++ lib/screens/video_call_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:trumeet/resources/auth_methods.dart';
+import 'package:trumeet/resources/jitsi_meet_methods.dart';
+import 'package:trumeet/utils/colors.dart';
+import 'package:trumeet/widgets/meeting_option.dart';
+
+class VideoCallScreen extends StatefulWidget {
+  VideoCallScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+  late TextEditingController meetingIdController;
+  late TextEditingController nameController;
+  bool isAudioMuted = true;
+  bool isVideoMuted = true;
+  final JitsiMeetMethods _jitsiMeetMethods = JitsiMeetMethods();
+
+  @override
+  void initState() {
+    meetingIdController = TextEditingController();
+    nameController = TextEditingController(text: _authMethods.user.displayName);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    meetingIdController.dispose();
+    nameController.dispose();
+    JitsiMeet.removeAllListeners();
+  }
+
+  _joinMeeting() {
+    _jitsiMeetMethods.createMeeting(
+        roomName: meetingIdController.text,
+        isAudioMuted: isAudioMuted,
+        isVideoMuted: isVideoMuted,
+        username: nameController.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'join a meeting',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: meetingIdController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: "room ID",
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: nameController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: "name",
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: _joinMeeting,
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                'join',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          MeetingOption(
+            text: 'mute audio',
+            isMute: isAudioMuted,
+            onChange: onAudioMuted,
+          ),
+          MeetingOption(
+              text: 'turn off video',
+              isMute: isAudioMuted,
+              onChange: onVideoMuted),
+        ],
+      ),
+    );
+  }
+
+  onAudioMuted(bool? val) {
+    setState(() {
+      isAudioMuted = val!;
+    });
+  }
+
+  onVideoMuted(bool? val) {
+    setState(() {
+      isVideoMuted = val!;
+    });
+  }
+}
